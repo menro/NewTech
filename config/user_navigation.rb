63c37f8ead8905308b9aef61ba32aa5f58012907
Salvatore Ferrucci <safeforge@gmail.com>
2012-09-02 15:26:20 +0200

    Add user menu items

--- config/user_navigation.rb
+++ config/user_navigation.rb
@@ -5,7 +5,13 @@ SimpleNavigation::Configuration.run do |navigation|
 
   navigation.items do |primary|
     if user_signed_in?
-      primary.item :account, content_tag(:i, nil, :class => 'icon-edit icon-large'), "#"
+      primary.item :account, current_user.username, "#" do |account|
+        account.item :account_profile, content_tag(:i, nil, :class => 'icon-user icon-large') << t(:'simple_navigation.account.profile'),
+                     "#"
+
+        account.item :account_logout, content_tag(:i, nil, :class => 'icon-off') << t(:'simple_navigation.account.logout'),
+                     destroy_user_session_path, :method => 'delete'
+      end
     end
     primary.dom_class = 'nav'
   end
