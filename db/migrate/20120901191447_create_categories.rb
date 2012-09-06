1bee88f8ca00d13de22f4cc9570a2335f593e963
Davide Papagni <davide@algonauti.com>
2012-09-06 08:04:04 +0200

    Integrated MarkerClusterer

--- db/migrate/20120901191447_create_categories.rb
+++ db/migrate/20120901191447_create_categories.rb
@@ -2,6 +2,7 @@ class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
       t.string      :code
+      t.string      :name
       t.timestamps
     end
   end
