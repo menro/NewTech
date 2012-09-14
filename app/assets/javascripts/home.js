3851c43df8248303448a303c14752cd6de63850d
Salvatore Ferrucci <safeforge@gmail.com>
2012-09-14 21:39:47 +0200

    fix merge issue

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -209,8 +209,8 @@
               $( "#years_range" ).html(ui.values[ 0 ] + " - " + ui.values[ 1 ] );
           },
           stop: function( event, ui ) {
-              $("#search_params").data("start_year", ui.values[ 0 ])
-              $("#search_params").data("end_year", ui.values[ 1])
+              $("#search_params").data("from_year", ui.values[ 0 ])
+              $("#search_params").data("to_year", ui.values[ 1])
               return $('.gmap').each(function() {
                   return refreshMap(this);
               });
