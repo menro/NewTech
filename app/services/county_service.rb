eae9c0bc42ec9d5ee87f6cce5568ae4b19ecb9fe
Salvatore Ferrucci <safeforge@gmail.com>
2012-09-15 18:56:06 +0200

    fix duplicates results

--- app/services/county_service.rb
+++ app/services/county_service.rb
@@ -5,6 +5,6 @@ class CountyService
     counties = County.scoped
     counties = counties.with_companies_founded_from params[:from_year] if params[:from_year].present?
     counties = counties.with_companies_founded_to params[:to_year] if params[:to_year].present?
-    CountyDecorator.decorate(counties, :search_params => params)
+    CountyDecorator.decorate(counties.uniq, :search_params => params)
   end
 end
--- app/services/office_service.rb
+++ app/services/office_service.rb
@@ -8,7 +8,7 @@ class OfficeService
     offices = Office.scoped
     offices = offices.with_companies_founded_from params[:from_year] if params[:from_year].present?
     offices = offices.with_companies_founded_to params[:to_year] if params[:to_year].present?
-    OfficeDecorator.decorate(offices)
+    OfficeDecorator.decorate(offices.uniq)
   end
 
 end
