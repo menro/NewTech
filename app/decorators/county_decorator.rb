eb95253eb57eeb35b6cf2de6acc56b7ecd7dcc49
Salvatore Ferrucci <safeforge@gmail.com>
2012-09-15 13:57:53 +0200

    Apply filter on total offices to improve percentage

--- app/decorators/county_decorator.rb
+++ app/decorators/county_decorator.rb
@@ -2,21 +2,29 @@ class CountyDecorator < Draper::Base
   decorates :county
 
   def offices_numbers
-    results = self.offices.count
-    unless search_params.nil?
-      results = self.offices.scoped
-      results = results.with_companies_founded_from search_params[:from_year] if search_params[:from_year].present?
-      results = results.with_companies_founded_to search_params[:to_year] if search_params[:to_year].present?
-    end
+    results = filter_offices_by_search_params(self.offices)
     results.count
   end
 
   def offices_percentage
-    ((offices_numbers * 100).to_f / Office.count.to_f).to_f
+    ((offices_numbers * 100).to_f / total_offices.to_f).to_f
   end
 
-
   private
+
+  def total_offices
+    filter_offices_by_search_params(Office).count
+  end
+
+  def filter_offices_by_search_params(offices)
+    results = offices.scoped
+    unless search_params.nil?
+      results = results.with_companies_founded_from search_params[:from_year] if search_params[:from_year].present?
+      results = results.with_companies_founded_to search_params[:to_year] if search_params[:to_year].present?
+    end
+    results
+  end
+
   def search_params
     @options[:search_params] ||= nil
   end
