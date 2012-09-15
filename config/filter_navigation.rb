b0ca56e24bfadec29c5d9adfb151317cf25197a2
Salvatore Ferrucci <safeforge@gmail.com>
2012-09-15 17:12:28 +0200

    add item on filter navigation

--- config/filter_navigation.rb
+++ config/filter_navigation.rb
@@ -4,20 +4,21 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.selected_class = 'active'
 
   navigation.items do |primary|
-    primary.item :employee, "Jobs", "#"
+    primary.item :jobs, "Jobs", "#"
     primary.item :employee, "Employee", "#" do |employee|
-      employee.item :one, "<10", "#"
-      employee.item :one, "11 - 25", "#"
-      employee.item :one, "26 - 75", "#"
-      employee.item :one, "76 . 250", "#"
-      employee.item :one, "> 250", "#"
+      employee.item :employee_one, "<10", "#"
+      employee.item :employee_two, "11 - 25", "#"
+      employee.item :employee_three, "26 - 75", "#"
+      employee.item :employee_for, "76 . 250", "#"
+      employee.item :employee_five, "> 250", "#"
     end
-    primary.item :employee, "Investment", "#" do |employee|
-      employee.item :one, "<10", "#"
-      employee.item :one, "11 - 25", "#"
-      employee.item :one, "26 - 75", "#"
-      employee.item :one, "76 . 250", "#"
-      employee.item :one, "> 250", "#"
+    primary.item :investment, "Investment", "#" do |investment|
+      investment.item :investment_one, "<10", "#"
+      investment.item :investment_two, "11 - 25", "#"
+      investment.item :investment_three, "26 - 75", "#"
+      investment.item :investment_for, "76 . 250", "#"
+      investment.item :investment_five, "> 250", "#"
+      investment.dom_class = 'dropup'
     end
     primary.dom_class = 'nav dropup'
   end
