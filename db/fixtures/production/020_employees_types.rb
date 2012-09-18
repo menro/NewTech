9d26a729dc00eaba3c17fbfd3038b1fde1cdec49
Salvatore Ferrucci <safeforge@gmail.com>
2012-09-19 00:31:15 +0200

    Add missing fixure in production environment

---
+++ db/fixtures/production/020_employees_types.rb
@@ -0,0 +1,15 @@
+EmployeesType.create do |employees|
+  employees.name = "<10"
+end
+EmployeesType.create do |employees|
+  employees.name = "11-25"
+end
+EmployeesType.create do |employees|
+  employees.name = "26-75"
+end
+EmployeesType.create do |employees|
+  employees.name = "76-250"
+end
+EmployeesType.create do |employees|
+  employees.name = ">251"
+end
---
+++ db/fixtures/production/030_investments_types.rb
@@ -0,0 +1,15 @@
+InvestmentsType.create do |investment|
+  investment.name = "<100,000"
+end
+InvestmentsType.create do |investment|
+  investment.name = "<500,000"
+end
+InvestmentsType.create do |investment|
+  investment.name = "<2M"
+end
+InvestmentsType.create do |investment|
+  investment.name = "<10M"
+end
+InvestmentsType.create do |investment|
+  investment.name = ">25M"
+end
