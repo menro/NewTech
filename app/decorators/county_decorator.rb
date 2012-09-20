6829f8d259fb7238fde3b2f30efa307c8d6a3ce2
Salvatore Ferrucci <safeforge@gmail.com>
2012-09-21 00:38:28 +0200

    Check nil params. fix npe

--- app/decorators/county_decorator.rb
+++ app/decorators/county_decorator.rb
@@ -20,12 +20,12 @@ class CountyDecorator < Draper::Base
   def filter_offices_by_search_params(offices)
     results = offices.scoped
     unless search_params.nil?
-      results = results.with_companies_founded_from search_params[:from_year] unless search_params[:from_year].empty?
-      results = results.with_companies_founded_to search_params[:to_year] unless search_params[:to_year].empty?
-      results = results.with_company_tagged_as search_params[:tag_code] unless search_params[:tag_code].empty?
-      results = results.with_company_are_hiring unless search_params[:hiring].empty?
-      results = results.with_company_employee_type(search_params[:employee_id]) unless search_params[:employee_id].empty?
-      results = results.with_company_investment_type(search_params[:investment_id]) unless search_params[:investment_id].empty?
+      results = results.with_companies_founded_from search_params[:from_year] unless search_params[:from_year].nil? || search_params[:from_year].empty?
+      results = results.with_companies_founded_to search_params[:to_year] unless search_params[:to_year].nil? || search_params[:to_year].empty?
+      results = results.with_company_tagged_as search_params[:tag_code] unless search_params[:tag_code].nil? || search_params[:tag_code].empty?
+      results = results.with_company_are_hiring unless search_params[:hiring].nil? || search_params[:hiring].empty?
+      results = results.with_company_employee_type(search_params[:employee_id]) unless search_params[:employee_id].nil? || search_params[:employee_id].empty?
+      results = results.with_company_investment_type(search_params[:investment_id]) unless search_params[:investment_id].nil? || search_params[:investment_id].empty?
     end
     results
   end
--- app/services/county_service.rb
+++ app/services/county_service.rb
@@ -3,12 +3,12 @@ class CountyService
 
   def self.search(params)
     counties = County.scoped
-    counties = counties.with_companies_founded_from params[:from_year] unless params[:from_year].empty?
-    counties = counties.with_companies_founded_to params[:to_year] unless params[:to_year].empty?
-    counties = counties.with_company_tagged_as params[:tag_code] unless params[:tag_code].empty?
-    counties = counties.with_company_are_hiring unless params[:hiring].empty?
-    counties = counties.with_company_employee_type(params[:employee_id]) unless params[:employee_id].empty?
-    counties = counties.with_company_investment_type(params[:investment_id]) unless params[:investment_id].empty?
+    counties = counties.with_companies_founded_from params[:from_year] unless params[:from_year].nil? || params[:from_year].empty?
+    counties = counties.with_companies_founded_to params[:to_year] unless params[:to_year].nil? || params[:to_year].empty?
+    counties = counties.with_company_tagged_as params[:tag_code] unless params[:tag_code].nil? || params[:tag_code].empty?
+    counties = counties.with_company_are_hiring unless params[:hiring].nil? || params[:hiring].empty?
+    counties = counties.with_company_employee_type(params[:employee_id]) unless params[:employee_id].nil? || params[:employee_id].empty?
+    counties = counties.with_company_investment_type(params[:investment_id]) unless params[:investment_id].nil? || params[:investment_id].empty?
     CountyDecorator.decorate(counties.uniq, :search_params => params)
   end
 end
