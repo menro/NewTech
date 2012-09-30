b73ca47c22455de88852116b8dcffff5113d317b
Salvatore Ferrucci <salvatore@algonauti.com>
2012-09-30 20:21:05 +0200

    admin companies and labels

--- app/admin/companies.rb
+++ app/admin/companies.rb
@@ -2,17 +2,21 @@ ActiveAdmin.register Company do
 
   # Create sections on the index screen
   scope :all, :default => true
-  scope :are_hiring
 
   filter :name
   filter :founded_year
 
   index do
-    column :id
     column :name
     column :founded_year
     column :enabled
     default_actions
   end
+
+  show do
+    render "show"
+  end
+
+  form :partial => "form"
 
 end
