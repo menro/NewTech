fb897c6b5c3e7c5f95ae4439d1e70354073b44ae
Salvatore Ferrucci <salvatore@algonauti.com>
2012-10-02 18:55:34 +0200

    remove duplication code and optimize son with less data

--- app/services/company_service.rb
+++ app/services/company_service.rb
@@ -61,25 +61,7 @@ class CompanyService
 
   def self.update(id, attributes = {})
     user = User.find(attributes[:user_id])
-    company = user.companies.find(id)
-    tags_list = attributes.delete("tags_list").split(",") rescue []
-    tags = []
-    tags_list.each do |tag|
-      tags << Tag.find_or_create_by_code(tag.strip)
-    end
-    company.tags = tags
-    geocode = find_geocode attributes[:offices_attributes]['0']
-    if !geocode.success || geocode.accuracy.to_i < 6
-      company.errors.add :base, "Address not founds"
-    elsif !( attributes[:offices_attributes]['0'][:zip_code].eql?(geocode.zip) )
-      company.errors.add :base, "Postal code not valid"
-    else
-      attributes[:offices_attributes]['0'][:address1] = geocode.street_address
-      attributes[:offices_attributes]['0'][:latitude] = geocode.lat
-      attributes[:offices_attributes]['0'][:longitude] = geocode.lng
-      company.update_attributes attributes
-    end
-    CompanyDecorator.new(company)
+    update_by_user(user, id, attributes)
   end
 
   def self.build(attributes = nil, options = {})
--- app/views/offices/index.json.rabl
+++ app/views/offices/index.json.rabl
@@ -1,10 +1,9 @@
 collection @offices, :object_root => false
 
-attributes :company_name, :company_permalink, :company_homepage_url, :company_description, :company_hiring,
-           :company_overview, :company_number_of_employees, :company_founded_year, :company_email_address,
-           :company_phone_number, :company_facebook, :company_twitter
+attributes :company_name, :company_homepage_url, :company_description, :company_hiring,
+           :company_number_of_employees, :company_founded_year
 
 attributes :company_image_url, :company_tags_list
 
-attributes :address1, :address2, :zip_code, :city, :state_code, :country_code, :latitude, :longitude
+attributes :latitude, :longitude
 
