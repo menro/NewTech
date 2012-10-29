b045114e8a8f489fb148281f7d5f4ecffe1f570b
Salvatore Ferrucci <salvatore@algonauti.com>
2012-10-29 22:53:12 +0100

    add tag filter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def welcome
-    @tags_for_cloud=TagService.tags_for_cloud
+    @tags=TagService.tags_for_cloud
     @categories=CategoryService.all
     @employees_types = EmployeesTypeService::all
     @investments_types = InvestmentsTypeService::all
