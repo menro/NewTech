8d687f008b6a3be863b085d03b3e4912f5f77d49
Salvatore Ferrucci <salvatore@algonauti.com>
2012-11-05 22:25:45 +0100

    add navigation links

--- config/navigation.rb
+++ config/navigation.rb
@@ -4,10 +4,10 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.selected_class = 'active'
 
   navigation.items do |primary|
-    primary.item :present, t(:'simple_navigation.present'), "#"
-    primary.item :message_board, t(:'simple_navigation.message_board'), "#"
-    primary.item :jobs_board, t(:'simple_navigation.jobs'), "#"
-    primary.item :blogs, t(:'simple_navigation.blogs'), "#"
+    primary.item :present, t(:'simple_navigation.present'), "https://spreadsheets.google.com/viewform?hl=en&formkey=dDk3VVRoU2k4OFRwRmRYdVdRYkdydFE6MA" , :link => {:target => '_blank'}
+    primary.item :message_board, t(:'simple_navigation.message_board'), "http://www.bdnewtech.com/messages/boards/", :link => {:target => '_blank'}
+    primary.item :jobs_board, t(:'simple_navigation.jobs'), "http://www.bdnewtech.com/messages/boards/forum/965517", :link => {:target => '_blank'}
+    primary.item :blogs, t(:'simple_navigation.blogs'), "http://www.bdnewtech.com/messages/boards/forum/1042687", :link => {:target => '_blank'}
     primary.dom_class = 'nav'
   end
 
