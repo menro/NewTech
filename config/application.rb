6ac7fe5574667baab633f9a3d176966d69a93a14
Salvatore Ferrucci <salvatore@algonauti.com>
2012-11-08 19:47:10 +0100

    don't initialize rails during assets precompile process

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Newtech
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # fix https://devcenter.heroku.com/articles/rails3x-asset-pipeline-cedar#troubleshooting
+    config.assets.initialize_on_precompile = false
   end
 end
