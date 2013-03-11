412f4481ea81420c462d7654627be1c02bc567d2
Nadeem Yasin <nadeem.yasin61@gmail.com>
2013-03-11 22:50:37 +0500

    For Sponsor image uplading

--- app/admin/sponsors.rb
+++ app/admin/sponsors.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register Sponsor do
-  
+
+  form :html => { :enctype => "multipart/form-data" } do |f|
+  f.inputs do
+    f.input :name
+    f.input :url
+    f.input :image, :multipart => true
+    
+  end
+  f.buttons
+end
+
 end
