24d6a2cba08624372cb5e473d93a88bd00401051
Nadeem Yasin <nadeem.yasin61@gmail.com>
2013-03-12 01:18:55 +0500

    Fixing for tablet issue

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -16,7 +16,13 @@
   GMap = (function() {
 
     function GMap(container) {
-        $(window).resize(function(){updateRecentBox();});
+        var supportsOrientationChange = "onorientationchange" in window,
+        orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
+
+        window.addEventListener(orientationEvent, function() {
+            updateRecentBox();
+        }, false);
+//        $(window).resize(function(){updateRecentBox();});
       // Initialize Google Map
       var defaultOptions;
       defaultOptions = {
