ff04141115bf6de81798e689648494b99d01c83c
Jay Zeschin <jay.zeschin@modeset.com>
2013-03-11 20:47:45 -0600

    Users should be able to edit job postings.
    Also ensure that job searching in controllers/services is appropriately scoped.

--- app/services/job_service.rb
+++ app/services/job_service.rb
@@ -5,7 +5,7 @@ class JobService
   end
 
   def self.find_all_by_user(user, params = {})
-    jobs = user.jobs.order("title ASC").page(params[:page])
+    jobs = user.jobs.order("expires_on DESC").page(params[:page])
     JobDecorator.decorate(jobs)
   end
 
@@ -26,12 +26,34 @@ class JobService
       attributes[:address] = geocode.street_address
       attributes[:latitude] = geocode.lat
       attributes[:longitude] = geocode.lng
-      company = Company.find_by_id(attributes.delete(:company_id))
+      attributes[:expires_on] = 30.days.from_now
+      company = user.companies.find_by_id(attributes.delete(:company_id))
       job = build(attributes)
       job.company = company
       job.save
     end
     JobDecorator.decorate(job)
+  end
+
+  def self.edit_by_user(user, id)
+    job = user.jobs.find(id)
+    JobDecorator.decorate(job)
+  end
+
+  def self.update_by_user(user, id, attributes = {})
+    job = user.jobs.find(id)
+    geocode = find_geocode attributes
+    if !geocode.success || geocode.accuracy.to_i < 6
+      job.errors.add :address, "Address not founds"
+    elsif !( attributes[:zip_code].eql?(geocode.zip) )
+      job.errors.add :zip_code, "Postal code not valid, google return address zipcode #{geocode.zip}"
+    else
+      attributes[:address] = geocode.street_address
+      attributes[:latitude] = geocode.lat
+      attributes[:longitude] = geocode.lng
+      job.update_attributes attributes
+    end
+    JobDecorator.new(job)
   end
 
   def self.find_geocode(params)
