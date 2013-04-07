51d6dde3fd8f8c1ad000343d6888639abbd465f4
Jay Zeschin <jay.zeschin@modeset.com>
2013-04-07 12:49:23 -0600

    Switch to Unicorn (#27)

---
+++ config/unicorn.rb
@@ -0,0 +1,48 @@
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 30
+preload_app true
+listen ENV['PORT'].to_i || 3000, tcp_nopush: false
+
+before_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+    Rails.logger.info('Disconnected from ActiveRecord')
+  end
+
+  if defined?(Resque)
+    Resque.redis.quit
+  end
+
+end
+
+after_fork do |server, worker|
+
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+    Rails.logger.info('Connected to ActiveRecord')
+  end
+
+  # If you are using Redis but not Resque, change this
+  if defined?(Resque)
+    uri = URI.parse(ENV["REDISTOGO_URL"])
+    Resque.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+    Rails.logger.info('Connected to Redis')
+  end
+
+  # Reset the object cache store (Dalli)
+  if Rails.cache.respond_to?(:reset)
+    Rails.cache.reset
+    Rails.logger.info('Rails cache has been reset')
+  end
+
+end
