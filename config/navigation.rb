dee4990c877a77635ae746336fd4ca81e19a4aeb
Nadeem Yasin <nadeem.yasin61@gmail.com>
2013-04-08 01:31:00 +0500

    Adding sponsor link in navigation

--- config/navigation.rb
+++ config/navigation.rb
@@ -7,6 +7,7 @@ SimpleNavigation::Configuration.run do |navigation|
     primary.item :present, t(:'simple_navigation.present'), "https://spreadsheets.google.com/viewform?hl=en&formkey=dDk3VVRoU2k4OFRwRmRYdVdRYkdydFE6MA" , :link => {:target => '_blank'}
     primary.item :message_board, t(:'simple_navigation.message_board'), "http://www.meetup.com/bdnewtech/messages/boards/", :link => {:target => '_blank'}
     primary.item :jobs_board, t(:'simple_navigation.jobs'), "http://www.meetup.com/bdnewtech/messages/boards/forum/965517", :link => {:target => '_blank'}
+    primary.item :sponsors, t(:'simple_navigation.sponsors'), sponsor_view_path, :link => {:target => '_blank'}
     primary.item :blogs, t(:'simple_navigation.blogs'), "http://www.meetup.com/bdnewtech/messages/boards/forum/1042687", :link => {:target => '_blank'}
     primary.dom_class = 'nav'
   end
