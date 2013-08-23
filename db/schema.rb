eddbf77a130d8a03e31b59e3a36476e676616dc2
Nadeem Yasin <nadeem.yasin61@gmail.com>
2013-08-24 01:16:18 +0500

    Attempt to fix bootstrap issue.

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
-ActiveRecord::Schema.define(:version => 20130819173455) do
+ActiveRecord::Schema.define(:version => 20130821181356) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
