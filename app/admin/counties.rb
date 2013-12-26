02df4dc35affb251a57ff304c33ef96757c0bbb4
Nadeem Yasin <nadeem.yasin61@gmail.com>
2013-12-26 23:20:27 +0500

    Adding state field in counties form in admin panel

--- app/admin/counties.rb
+++ app/admin/counties.rb
@@ -1,2 +1,12 @@
 ActiveAdmin.register County  do
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :state, as: :select, collection: StateService::all
+      f.input :longitude
+      f.input :latitude
+    end
+
+    f.buttons
+  end
 end
