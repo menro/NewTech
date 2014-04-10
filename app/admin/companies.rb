48dfd5503df5bd6869c030724db0d63bbf6c121b
Nadeem Yasin <nadeem.yasin61@gmail.com>
2014-04-10 23:28:04 +0500

    Re-made companies show page in admin panel. Also changing 9,000 ot 10,000 in view side.

--- app/admin/companies.rb
+++ app/admin/companies.rb
@@ -15,6 +15,44 @@ ActiveAdmin.register Company  do
     default_actions
   end
 
+  show do |company|
+    attributes_table do
+      row :id
+      row :name
+      row :user
+      row :city
+      row :county
+      row :zipcode do 
+        link_to company.zipcode.code, admin_zipcode_path(company.zipcode_id)
+      end
+      row :category
+      row :raising_money
+      row :money_raisig_expired_at
+      row :homepage_url
+      row :facebook
+      row :twitter
+      row :jobs_url
+      row :address
+      row :address2
+      row :founded_year
+      row :email_address
+      row :phone_number
+      row :description
+      row :overview
+      row :latitude
+      row :longitude
+      row :jobs_count
+      row :kickstarter_url
+      row :kickstarter_end_date
+      row :permalink
+      row :enabled
+      row :presented
+      row :presentation_date
+      row :created_at
+      row :updated_at
+    end
+  end
+
   controller do
 
     def edit
